857482a4c7ce54d1688d2d94ff5b328563bff844
Chhuong Le <chhuongcle@gmail.com>
2021-11-04 15:14:50 -0700

    made changes to icons and letter sizing on landing page

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { IconContext } from 'react-icons';
 import  { AiOutlineHtml5 } from 'react-icons/ai';
 import { FaCss3Alt, FaReact, FaNodeJs, FaAws, FaDocker} from 'react-icons/fa'
 import { SiJavascript,
@@ -23,41 +24,41 @@ const Landing = () => {
         <div className="job">a full-stack engineer</div>
       </div>
       <div className="meContainer">
-        <div>Hi! My name is Chhuong (pronounced "CHONG") and I am a full stack engineer based in San Fransico, CA.
+        <div className="bio">Hi! My name is Chhuong (pronounced "CHONG") and I am a full stack engineer based in San Fransico, CA.
           I love taking landscape photographs and doing sports. I am also a cat dad to the most adorable ragdoll, Loki!</div>
         <div className="dots">. . . . .</div>
       </div>
       <div className="techContainer">
-        <h2>My Tech Stack</h2>
+        <h2 className="bio-title">My Tech Stack</h2>
         <div>
-          <h3>Front End:</h3>
-          <div className="skillIcons">
-            <AiOutlineHtml5 size={70}/>
-            <FaCss3Alt  size={70}/>
-            <SiJavascript  size={70}/>
-            <FaReact  size={70}/>
-          </div>
+          <h3 className="stack-title">Front End:</h3>
+          <IconContext.Provider value={{size: "3.25vw"}}>
+            <AiOutlineHtml5 />
+            <FaCss3Alt />
+            <SiJavascript />
+            <FaReact />
+          </IconContext.Provider>
         </div>
         <div>
-          <h3>Back End:</h3>
-          <div className="skillIcons">
-            <SiMysql  size={70}/>
-            <SiPostgresql  size={70}/>
-            <SiMongodb  size={70}/>
-            <FaNodeJs  size={70}/>
-          </div>
+          <h3 className="stack-title">Back End:</h3>
+          <IconContext.Provider value={{size: "3.25vw"}} >
+            <SiMysql />
+            <SiPostgresql />
+            <SiMongodb />
+            <FaNodeJs />
+          </IconContext.Provider>
         </div>
         <div>
-          <h3>Other tools:</h3>
-          <div className="skillIcons">
-            <SiJira  size={70}/>
-            <FaAws  size={70}/>
-            <SiBabel  size={70}/>
-            <SiWebpack  size={70}/>
-            <FaDocker  size={70}/>
-            <SiNginx  size={70}/>
-            <SiFirebase  size={70}/>
-          </div>
+          <h3 className="stack-title">Other tools:</h3>
+          <IconContext.Provider value={{size:"3.25vw"}}>
+            <SiJira />
+            <FaAws />
+            <SiBabel />
+            <SiWebpack />
+            <FaDocker />
+            <SiNginx />
+            <SiFirebase />
+          </IconContext.Provider>
         </div>
       </div>
       <div className="contactMeContainer">
