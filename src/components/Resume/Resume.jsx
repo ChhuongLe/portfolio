87b0f4f3749158be8df4fa8f37ef1da9670b4900
Chhuong Le <chhuongle@gmail.com>
2021-11-18 17:04:35 -0800

    add background color to each section and increase size of images

--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -27,20 +27,25 @@ const About = () => {
         <div className="about-bio">
           <h2 className="header">About Me</h2>
           <div className="bio-description">
-            <h4 className="section-header">Education:</h4>
-              I graduated from the <b>University of California, Riverside</b> back in 2016 as a <b>Chemical
-              and Environmental Engineering</b> major and also recieved my minor in <b>Computer Science</b>.
-              I was a part of the American Institue of Chemical Engineers, Engineering without Borders, and
-              Tennis on Campus during my time there.<br/>
+            <div className="section-background">
+              <h4 className="section-header">Education:</h4>
+                I graduated from the <b>University of California, Riverside</b> back in 2016 as a <b>Chemical
+                and Environmental Engineering</b> major and also recieved my minor in <b>Computer Science</b>.
+                I was a part of the American Institue of Chemical Engineers, Engineering without Borders, and
+                Tennis on Campus during my time there.<br/>
+             </div>
             <div className="seperator"> . . . . . .</div>
-            <h4 className="section-header">Why Software Engineering?</h4>
-              Ah yes, why <b>software engineering</b>? While working my first job out of university,
-              I was in charge of developing a product that dealt with data collection and embedded systems
-              for trouble shooting and calibrating the oven's that the company manufactured. The more the
-              I was moving forward with the project, the more I fell in love with designing and creating the
-              software to better the user experience. As the project came to a close, I began missing the
-              software design that I was doing and decided to full send my career towards becoming a software engineer.
+            <div className="section-background">
+              <h4 className="section-header">Why Software Engineering?</h4>
+                Ah yes, why <b>software engineering</b>? While working my first job out of university,
+                I was in charge of developing a product that dealt with data collection and embedded systems
+                for trouble shooting and calibrating the oven's that the company manufactured. The more the
+                I was moving forward with the project, the more I fell in love with designing and creating the
+                software to better the user experience. As the project came to a close, I began missing the
+                software design that I was doing and decided to full send my career towards becoming a software engineer.
+            </div>
             <div className="seperator"> . . . . . .</div>
+            <div className="section-background">
             <h4 className="section-header">Hobbies:</h4>
               For hobbies, I always describe myself as a student. I simply love learning new things all the time,
               whether it be things like cooking new dishes or creating small projects for games that I play.
@@ -54,6 +59,7 @@ const About = () => {
               the cities and countries that I visit! Check out my instagram for some of my shots! <br/>
               <a className="insta-bio" href="https://www.instagram.com/supawok/"><AiOutlineInstagram size={50}/> <br/></a>
               I'm still developing my style of shooting so please feel free to give me some pointers. <br />
+            </div>
           </div>
         </div>
       </div>
